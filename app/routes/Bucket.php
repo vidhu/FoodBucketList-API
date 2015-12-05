0cc8e3024ce515f8e3002478ca81f69d74de6c54
vidhu <vidhu@bu.edu>
2015-12-04 22:22:19 -0500

    Added Bucket route. Not yet implemented

--- app/routes/Bucket.php
+++ app/routes/Bucket.php
@@ -1,9 +1,19 @@
 <?php
 
 $app->get('/bucket/{id}', function($req, $res, $args){
+    //$db = $this->getContainer()->get('DB');
     
-    $db = $this->getContainer()->get('DB');
     
-    echo "hi!";
+    echo "Return bucket list";
 });
 
+$app->post('/bucket', function($req, $res, $args){
+    if(empty($_POST['bucket-name'])){
+        echo "back-name should be specified";
+        return $res->withStatus(400);
+    }
+    $db = $this->getContainer()->get('DB');
+    $bucketName = $_POST['bucket-name'];
+    
+    echo "Create new bucket list with name: $bucketName";
+});
