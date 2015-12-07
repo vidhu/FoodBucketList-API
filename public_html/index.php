a0fc124848dad4b22c3a50e2f4f11de3b4ee8774
vidhu <vidhu@bu.edu>
2015-12-06 23:21:15 -0500

    Allowed DELETE http verb

--- public_html/index.php
+++ public_html/index.php
@@ -50,9 +50,9 @@ $app->add(function($req, $res, $next){
     /* @var $res \Slim\Http\Response */
     $res = $next($req, $res);
     $res = $res->withHeader('Content-type', 'application/json');
-    $res = $res->withHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     $res = $res->withHeader('Access-Control-Allow-Origin', '*');
     $res = $res->withHeader('Access-Control-Max-Age', '1000');
+    $res = $res->withHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     
     return $res;
 });
