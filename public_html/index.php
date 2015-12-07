a65f95e43c2002fd4b9c88088606cec3915b4734
vidhu <vidhu@bu.edu>
2015-12-06 23:30:28 -0500

    Allowed DELETE http verb

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 require_once '../app/vendor/autoload.php';
 require_once '../app/middleware/FBAuthMiddleWare.php';
